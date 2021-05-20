09e48863ad263fbdc3fa79342fafc23ba93f617e
Hassan Shakeri <shakeri.hassan@zoho.com>
2021-05-20 13:36:46 +0200

    removed the close button

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const open = document.querySelector(".open-btn");
-const close = document.querySelector(".close-btn");
 const overlay = document.querySelector(".overlay");
 
 open.addEventListener("click",()=>{
     overlay.classList.add("open-overlay")
 })
 
-close.addEventListener("click",()=>{
+window.addEventListener("keydown", e =>{
+    if (e.key == "Escape"){
     overlay.classList.remove("open-overlay")
+}
 })
